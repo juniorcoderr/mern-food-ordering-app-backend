f15cdc7b4b783189f19d4d2c0220600e336f8e58
Vinayak Yadav <netgoluyadav337@gmail.com>
2024-11-18 22:57:06 +0530

    adding checkout flow

---
+++ src/routes/OrderRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import OrderController from "../controllers/OrderController";
+
+const router = express.Router();
+
+router.post(
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession as express.RequestHandler
+);
+
+router.post(
+  "/checkout/webhook",
+  OrderController.stripeWebhookHandler as express.RequestHandler
+);
+
+export default router;
